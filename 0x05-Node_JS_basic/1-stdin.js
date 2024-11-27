90eb3f5443cc6f60b687eb43595694935138e5f8
Nyagarama <mochristopher27@gmail.com>
2024-11-27 16:50:49 +0000

    Use process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 // setup the readline interface
 const queryline = require('readline').createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
- 
+
 // check if we are in interactive mode (accepting input from the keyboard)
 const inInteractiveMode = process.stdin.isTTY;
- 
+
 if (inInteractiveMode) {
   // print the first message and accept the name as input and print the name
   queryline.question('Welcome to Holberton School, what is your name?\n', (yourName) => {
@@ -19,7 +19,7 @@ if (inInteractiveMode) {
   process.stdin.on('data', (character) => {
     name += character.toString().trim();
   });
- 
+
   process.stdin.on('end', () => {
     console.log(`Your name is: ${name}`);
     queryline.close();
